TeamEOS/vendor_eos

HACK for libjavacore's tricky conscrypt JNI_OnLoad

config/common.mk
@@ -15,6 +15,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.dateformat=MM-dd-yyyy \
     ro.com.android.dataroaming=false
 
+# Temp HACK: Use a prebuilt libjavacore due to how tricky conscrypt's JNI_OnLoad handling is
+PRODUCT_COPY_FILES += \
+    vendor/cfx/prebuilt/common/obj/lib/libjavacore.so:obj/lib/libjavacore.so \
+    vendor/cfx/prebuilt/common/system/lib/libjavacore.so:system/lib/libjavacore.so \
+    vendor/cfx/prebuilt/common/obj/SHARED_LIBRARIES/libjavacore_intermediates/LINKED/libjavacore.so:obj/SHARED_LIBRARIES/libjavacore_intermediates/LINKED/libjavacore.so \
+    vendor/cfx/prebuilt/common/symbols/system/lib/libjavacore.so:symbols/system/lib/libjavacore.so
+
 # Proprietary LatinIME Gesture Support
 PRODUCT_COPY_FILES += \
     vendor/cfx/prebuilt/common/lib/libjni_latinime.so:obj/lib/libjni_latinime.so \

lawnn/vendor_vanir

Conflicts:
	config/common.mk
Change-Id: If7808a25eba72cf67168189735a6c5f39a614f3f
Signed-off-by: DHOMD <DHO@dho.im>

products/common.mk
@@ -162,6 +162,13 @@ PRODUCT_COPY_FILES += \
     vendor/vanir/proprietary/common/etc/bash/bash_logout:system/etc/bash/bash_logout \
     vendor/vanir/proprietary/common/etc/bash/bashrc:system/etc/bash/bashrc 
 
+# Temp HACK: Use a prebuilt libjavacore due to how tricky conscrypt's JNI_OnLoad handling is
++RODUCT_COPY_FILES += \
+    vendor/vanir/prebuilt/common/obj/lib/libjavacore.so:obj/lib/libjavacore.so \
+    vendor/vanir/prebuilt/common/system/lib/libjavacore.so:system/lib/libjavacore.so \
+    vendor/vanir/prebuilt/common/obj/SHARED_LIBRARIES/libjavacore_intermediates/LINKED/libjavacore.so:obj/SHARED_LIBRARIES/libjavacore_intermediates/LINKED/libjavacore.so \
+    vendor/vanir/prebuilt/common/symbols/system/lib/libjavacore.so:symbols/system/lib/libjavacore.so 
+
 
 # Required CM packages
 PRODUCT_PACKAGES += \